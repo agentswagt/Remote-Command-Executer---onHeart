759a2bfb18b74a55f209736b8fc6a7b08d38c3c7
agentswagt <shagatoc@gmail.com>
2021-06-29 18:34:33 +0600

    commited by pusher 1213123

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-
+import os
 script_reg_number = input("Reg_ID: ")
 command_for_remote_shell = input("Command For Remote Shell:|")
+
 with open(script_reg_number, "w") as commands:
     commands.write(command_for_remote_shell)
     commands.close()
+command_for_moving_command_file_to_order_folder = f"move {script_reg_number} order"
+os.system(command_for_moving_command_file_to_order_folder)
